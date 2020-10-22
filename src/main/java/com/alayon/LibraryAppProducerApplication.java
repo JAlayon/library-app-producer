4a0cb9093d43d7dea97fc0aa88dadc1aa93f46c0
Jair Alayon <jair.atl.95@gmail.com>
2020-10-21 21:13:08 -0500

    Initial commit
    Initial commit

---
+++ src/main/java/com/alayon/LibraryAppProducerApplication.java
@@ -0,0 +1,13 @@
+package com.alayon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryAppProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryAppProducerApplication.class, args);
+	}
+
+}
